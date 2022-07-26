5be1d908e1abe25c1cba3e52549e790c0927d19a
Lucas Rodriguez <lucasg29r95@gmail.com>
2022-07-26 01:00:28 -0300

    Se modifico el cors global.

--- src/main/java/com/back/portfolio/PortfolioApplication.java
+++ src/main/java/com/back/portfolio/PortfolioApplication.java
@@ -18,7 +18,7 @@ public class PortfolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://apportfolioweb.web.app/").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
